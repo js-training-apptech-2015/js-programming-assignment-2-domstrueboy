53f6902daafae11e0c29b0cd21c1658f1bf071db
domstrueboy <domstrueboy@rambler.ru>
2015-11-15 03:32:55 +0500

    fixed solution2 a bit (remove unusal comments)

--- solution2.js
+++ solution2.js
@@ -7,14 +7,8 @@ function solution2(S){
                        elem === '"' || elem === "'"
               );
 
-//  var buffer = [];
-
   var len = S.length;
-  /*for(var i = 0; i < len; i++){
-    if(S[i] === '[' || S[i] === '{' || S[i] === '(' || S[i] === '"' || S[i] === "'"){
-      count++;
-    }
-  }*/
+
   S = S.join('');
 
   for(var i = 0; i < len/2; i++){
